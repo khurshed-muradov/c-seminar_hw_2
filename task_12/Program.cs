0eef66b7ceeaa992375e47c7866a89b435d64e65
Khurshed Muradov <hurshed@gmail.com>
2024-07-21 17:56:59 +0500

    added task_12

--- task_12/Program.cs
+++ task_12/Program.cs
@@ -4,3 +4,19 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+System.Console.WriteLine("Введите число: ");
+int weekDay = int.Parse(Console.ReadLine()!);
+while (weekDay <= 7)
+{
+    if (weekDay <= 5)
+    {
+        System.Console.WriteLine("Нет");
+        break;
+    }
+    else
+    {
+        System.Console.WriteLine("Да");
+        break;
+    }
+
+}
