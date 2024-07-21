8a7dab8c6ff5189b19467590ec6c18b83a8cbc53
Khurshed Muradov <hurshed@gmail.com>
2024-07-21 17:38:31 +0500

    added task_11

--- task_11/Program.cs
+++ task_11/Program.cs
@@ -4,3 +4,22 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+while (true)
+{
+    int im_userInput = int.Parse(Console.ReadLine()!);
+    if (im_userInput < 100)
+    {
+        System.Console.Write(im_userInput + " -> третьей цифры нет");
+        break;
+    }
+    if (im_userInput < 1000)
+    {
+        System.Console.Write(im_userInput + " -> " + (im_userInput % 10));
+        break;
+    }
+    if (im_userInput < 10000)
+    {
+        System.Console.Write(im_userInput + " -> " + (((im_userInput % 100) / 10)));
+        break;
+    }
+}
