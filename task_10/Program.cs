2fcaecb47b4c7543baed29c445f95518d65fba1a
Khurshed Muradov <hurshed@gmail.com>
2024-07-21 17:14:55 +0500

    added task_10

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -4,3 +4,33 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+int UserInput()
+{
+    while (true)
+    {
+        int userInput = int.Parse(Console.ReadLine()!);
+        if (userInput >= 100 && userInput < 1000)
+        {
+            return userInput;
+        }
+        else
+        {
+            System.Console.WriteLine("Число не трехзначное! Пожалуйста, введите трехзначное число.");
+
+        }
+    }
+}
+int userInput = UserInput();
+int firstDigit = userInput / 100;
+int secondDigit = (userInput % 100) / 10;
+int thirdDigit = userInput % 10;
+
+System.Console.WriteLine(userInput + " -> " + secondDigit);
+
+
+
+
+
+
+
