5a641475ed1f1529c2d0444ec1d0b8f11c74e22c
Khurshed Muradov <hurshed@gmail.com>
2024-08-02 08:14:39 +0500

    added hw

---
+++ task_11_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+string mess = "Введите число: ";
+int number1 = Prompt(mess);
+int result = FindThirdNumber(number1);
+if (result == -1)
+{
+    System.Console.WriteLine("Такой цифры нет!");
+}
+else
+{
+    System.Console.WriteLine($"Третья цифра числа {result}");
+}
+
+int Prompt(string message)
+{
+    Console.Clear();
+    Console.WriteLine(message);
+    int num = Math.Abs(int.Parse(Console.ReadLine()));
+    return num;
+}
+
+int FindThirdNumber(int num)
+{
+    if (num < 100)
+    {
+        return -1;
+    }
+    else
+    {
+        while (num >= 1000)
+        {
+            num = num / 10;
+        }
+        int thirdNumber = num % 10;
+        return thirdNumber;
+    }
+}
